d7fa66648d31efcab49dd3ac42513356bdc76dd9
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-07 18:54:29 -0300

    realizando queries com o Knex

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
@@ -6,6 +7,18 @@ const app = fastify()
 app.get('/hello', async () => {
   const tables = await knex('sqlite_schema').select('*')
   return tables
+})
+
+app.get('/transactions', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
