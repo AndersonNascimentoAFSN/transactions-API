9ea69db8ca258f31aceecd83dde8567bd64bb3a7
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-07 19:42:11 -0300

    add plugins do Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,10 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
-})
-
-app.get('/transactions', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*')
-
-  return transaction
-})
+app.register(transactionsRoutes)
 
 app
   .listen({
