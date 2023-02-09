07177a43b99a1bd57f884500e880cebba72f4805
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-02-09 01:09:34 -0300

    testando listagem de transações

--- test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { describe, expect, it } from 'vitest'
-
-describe('ok', () => {
-  it('o usuário consegue criar uma nova transação', () => {
-    const responseStatusCode = 201
-
-    expect(responseStatusCode).toEqual(201)
-  })
-})
